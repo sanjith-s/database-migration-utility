f0a3386496621d5ad146d9584b2a7b2376dfe51f
sanjith-s <sanjithsenthilmurugan@gmail.com>
2023-07-30 22:44:40 +0530

    Added JavaDoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.sanjith.dbmigrator"},{"l":"com.sanjith.dbmigrator.config"},{"l":"com.sanjith.dbmigrator.dao"},{"l":"com.sanjith.dbmigrator.service"}];updateSearchResults();
